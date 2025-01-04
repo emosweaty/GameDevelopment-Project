fa8ce8bf9120e4021d840bb870fbe965c70c0239
emosweaty <ymkeschoofs04@gmail.com>
2025-01-04 02:39:36 +0100

    Added Screen as abstract class

---
+++ GameDevelopment_SchoofsYmke/Display/Screen.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevelopment_SchoofsYmke.Display
+{
+    abstract class Screen
+    {
+        protected Texture2D backgroundTexture;
+        protected SpriteFont font;
+        protected SpriteBatch spriteBatch;
+        protected Texture2D button;
+
+        public Screen(SpriteBatch spriteBatch, Texture2D backgroundTexture, Texture2D button, SpriteFont font)
+        {
+            this.spriteBatch = spriteBatch;
+            this.backgroundTexture = backgroundTexture;
+            this.font = font;
+            this.button = button;
+        }
+
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
+    }
+}
