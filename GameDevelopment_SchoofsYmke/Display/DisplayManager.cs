56813646287afe3e393390615e8ed1355d29ff09
emosweaty <ymkeschoofs04@gmail.com>
2025-01-06 21:27:34 +0100

    Refactored to a Singleton

--- GameDevelopment_SchoofsYmke/Display/DisplayManager.cs
+++ GameDevelopment_SchoofsYmke/Display/DisplayManager.cs
@@ -11,25 +11,34 @@ namespace GameDevelopment_SchoofsYmke.Display
 {
     internal class DisplayManager
     {
+        private static DisplayManager instance;
+
         private GraphicsDeviceManager graphics;
 
-        public DisplayManager(GraphicsDeviceManager graphics) 
+        private DisplayManager(GraphicsDeviceManager graphics)
         {
             this.graphics = graphics;
         }
 
+        public static DisplayManager GetInstance(GraphicsDeviceManager graphics)
+        {
+            if (instance == null)
+            {
+                instance = new DisplayManager(graphics);
+            }
+            return instance;
+        }
+
         public void Apply()
         {
-            graphics.PreferredBackBufferWidth = 2550;
-            graphics.PreferredBackBufferHeight = 1500;
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
 
-            //graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-            //graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-            graphics.IsFullScreen = false;
+            graphics.IsFullScreen = true;
             graphics.ApplyChanges();
         }
 
-        public int ScreenWidth => 2550;
-        public int ScreenHeight => 1500;
+        public int ScreenWidth => GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        public int ScreenHeight => GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
     }
 }
